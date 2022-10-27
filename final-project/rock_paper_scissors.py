be4c11ddf3e7f6be9bfc942549cf15ade6b37c92
robinsonbrennen <rob16073@byui.edu>
2022-10-26 19:43:38 -0600

    final touches

--- final-project/rock_paper_scissors.py
+++ final-project/rock_paper_scissors.py
@@ -1,6 +1,7 @@
 import random
 from enum import IntEnum
 
+# Create class input and use intenum to create enumerated constants
 class Input(IntEnum):
     Rock = 0
     Paper = 1
@@ -8,24 +9,28 @@ class Input(IntEnum):
     Lizard = 3
     Spock = 4
 
+# Add all of the winning actions
 win = {
     Input.Scissors: [Input.Lizard, Input.Paper], Input.Paper: [Input.Spock, Input.Rock], Input.Rock: [Input.Lizard, Input.Scissors], Input.Lizard: [Input.Spock, Input.Paper], Input.Spock: [Input.Scissors, Input.Rock]
 }
 
+# Create a dictionary to define the ways to win
 def pick_winner(user_pick, computer_pick):
     defeats = win[user_pick]
     if user_pick == computer_pick:
-        print(f"Both players selected {user_pick.name}. It's a tie!")
+        print(f"You both picked {user_pick.name}. It's a tie!")
     elif computer_pick in defeats:
         print(f"{user_pick.name} beats {computer_pick.name}! You win!")
     else:
         print(f"{computer_pick.name} beats {user_pick.name}! You lose.")
 
+# Get input from the computer at random
 def computer_input():
     selection = random.randint(0, len(Input) - 1)
     action = Input(selection)
     return action
 
+# Get input from user
 def user_input():
     choices = [f"{action.name}[{action.value}]" for action in Input]
     choices_str = ", ".join(choices)
@@ -33,6 +38,7 @@ def user_input():
     action = Input(selection)
     return action
 
+# While loop to direct the game, check for valid input, and ask user if they want to play again
 while True:
     try:
         user_pick = user_input()
