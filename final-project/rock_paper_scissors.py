3e6996cae0b7bc2f846af557b55cbe288338dff0
robinsonbrennen <rob16073@byui.edu>
2022-10-26 20:20:33 -0600

    final commit

--- final-project/rock_paper_scissors.py
+++ final-project/rock_paper_scissors.py
@@ -1,3 +1,4 @@
+# "Scissors cuts paper, paper covers rock, rock crushes lizard, lizard poisons Spock, Spock smashes scissors, scissors decapitates lizard, lizard eats paper, paper disproves Spock, Spock vaporizes rock, and as it always has, rock crushes scissors."
 import random
 from enum import IntEnum
 
